2375c089c419e22dd6bf0e5e8c01dee4307486ff
EstebanDrummer <yepesk8r@gmail.com>
2013-04-13 15:26:25 -0500

    con caracteres
    caracteres

--- gen/com/seminarioAndroid/pyamba/R.java
+++ gen/com/seminarioAndroid/pyamba/R.java
@@ -21,9 +21,10 @@ public final class R {
     }
     public static final class id {
         public static final int LinearLayout1=0x7f080000;
-        public static final int buttonUpdate=0x7f080002;
-        public static final int editText=0x7f080001;
-        public static final int menu_settings=0x7f080003;
+        public static final int buttonUpdate=0x7f080003;
+        public static final int editText=0x7f080002;
+        public static final int menu_settings=0x7f080004;
+        public static final int textCount=0x7f080001;
     }
     public static final class layout {
         public static final int activity_status=0x7f030000;
@@ -34,6 +35,7 @@ public final class R {
     public static final class string {
         public static final int app_name=0x7f050000;
         public static final int buttonUpdate=0x7f050006;
+        public static final int counterInitValue=0x7f050007;
         public static final int hello_world=0x7f050001;
         public static final int hintText=0x7f050005;
         public static final int menu_settings=0x7f050002;
--- src/com/seminarioAndroid/pyamba/StatusActivity.java
+++ src/com/seminarioAndroid/pyamba/StatusActivity.java
@@ -1,8 +1,13 @@
 package com.seminarioAndroid.pyamba;
 
 import winterwell.jtwitter.Twitter;
+import winterwell.jtwitter.TwitterException;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Activity;
+import android.graphics.Color;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -10,14 +15,17 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 import android.support.v4.app.NavUtils;
 
-public class StatusActivity extends Activity implements OnClickListener{
+public class StatusActivity extends Activity implements OnClickListener, TextWatcher{
 
 	private static final String TAG ="StatusActivity";
 	EditText editText;
 	Button updateButton;
 	Twitter twitter;
+	TextView textCount;
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -26,13 +34,43 @@ public class StatusActivity extends Activity implements OnClickListener{
         
         editText =(EditText)findViewById(R.id.editText);
         updateButton=(Button)findViewById(R.id.buttonUpdate);
-        
         updateButton.setOnClickListener(this);
+        
+        //caracteres restantes
+        textCount=(TextView)findViewById(R.id.textCount);
+        textCount.setText(Integer.toString(140));
+        textCount.setTextColor(Color.GREEN);
+        editText.addTextChangedListener(this);
         
         Log.d(TAG, "Create Twitter object");
         twitter = new Twitter("student","password");
         twitter.setAPIRootUrl("http://yamba.marakana.com/api");
         Log.d(TAG, "Set twitter object API root URL");
+    }
+    //asincronamente
+    
+    class PostToTwitter extends AsyncTask<String, Integer, String>{
+
+		@Override
+		protected String doInBackground(String... statuses) {
+			// TODO Auto-generated method stub
+			try{
+				Twitter.Status status =twitter.updateStatus(statuses[0]);
+				return status.text;
+			}catch(TwitterException e){
+				Log.e(TAG,e.toString());
+				e.printStackTrace();
+				return "Failed to post";
+			}
+		}
+		@Override
+		protected void onProgressUpdate(Integer...values) {
+			super.onProgressUpdate(values);
+		}
+		@Override
+		protected void onPostExecute(String result) {
+			Toast.makeText(StatusActivity.this, result, Toast.LENGTH_LONG).show();
+		}
     }
 
     @Override
@@ -44,8 +82,34 @@ public class StatusActivity extends Activity implements OnClickListener{
 	@Override
 	public void onClick(View v) {
 		// TODO Auto-generated method stub
-		twitter.setStatus(editText.getText().toString());
-		Log.d(TAG, "onClicked");
+String status =editText.getText().toString();
+new PostToTwitter().execute(status);
+Log.d(TAG, "onClicked");
+	}
+
+	@Override
+	public void afterTextChanged(Editable statusText) {
+		// TODO Auto-generated method stub
+		int count =140 - statusText.length();
+		textCount.setText(Integer.toString(count));
+		textCount.setTextColor(Color.GREEN);
+		if (count <10 )
+			textCount.setTextColor(Color.YELLOW);
+		if(count < 0 )
+			textCount.setTextColor(Color.RED);
+	}
+
+	@Override
+	public void beforeTextChanged(CharSequence s, int start, int count,
+			int after) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onTextChanged(CharSequence s, int start, int before, int count) {
+		// TODO Auto-generated method stub
+		
 	}
 
     
